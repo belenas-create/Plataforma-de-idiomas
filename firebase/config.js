1f84863ac7aabadce3144927e7c8afea9202b779
belenas-create <beatrizhelenas@gmail.com>
2026-08-16 16:12:26 -0300

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+js
+const firebaseConfig = {
+  apiKey: "AIzaSyA5HLkEBeewPsvW3URKSNZvN8O8-ANE5zo",
+  authDomain: "painel-de-idiomas.firebaseapp.com",
+  projectId: "painel-de-idiomas",
+  storageBucket: "painel-de-idiomas.firebasestorage.app",
+  messagingSenderId: "311687414371",
+  appId: "1:311687414371:web:4469d400b3b7c289bfae56"
+};
+
+if (typeof window !== "undefined") window.firebaseConfig = firebaseConfig;
